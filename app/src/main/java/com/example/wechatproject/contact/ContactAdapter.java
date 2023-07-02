6d697bf2806bff1f49519bc3ba0ac88b50c642fb
jintaixi <sofiacabello666@outlook.com>
2023-07-02 13:25:43 +0800

    补充文件

---
+++ app/src/main/java/com/example/wechatproject/contact/ContactAdapter.java
@@ -0,0 +1,71 @@
+package com.example.wechatproject.contact;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.wechatproject.R;
+import com.example.wechatproject.contact.ContactItem;
+import com.example.wechatproject.message.ChatActivity;
+
+import java.util.List;
+
+public class ContactAdapter extends ArrayAdapter<ContactItem> {
+    private List<ContactItem> contactItemList;
+    private LayoutInflater inflater;
+
+    public ContactAdapter(Context context, int resource, List<ContactItem> objects) {
+        super(context, resource, objects);
+        contactItemList = objects;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.contact_list_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.avatarImageView = convertView.findViewById(R.id.avatarImageView);
+            viewHolder.userNameTextView = convertView.findViewById(R.id.userNameTextView);
+            viewHolder.signatureTextView = convertView.findViewById(R.id.signatureTextView);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // 获取当前位置的联系人项
+        ContactItem contactItem = contactItemList.get(position);
+
+        // 在视图中显示联系人数据
+        viewHolder.avatarImageView.setImageURI(Uri.parse(contactItem.getAvatarFilePath()));
+        viewHolder.userNameTextView.setText(contactItem.getUserName());
+        viewHolder.signatureTextView.setText(contactItem.getSignature());
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ChatActivity.class);
+                intent.putExtra("name", contactItem.getUserName());
+                intent.putExtra("avatarFilePath", contactItem.getAvatarFilePath());
+                getContext().startActivity(intent);
+            }
+        });
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView avatarImageView;
+        TextView userNameTextView;
+        TextView signatureTextView;
+    }
+}
+
