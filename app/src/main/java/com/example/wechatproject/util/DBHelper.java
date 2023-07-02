eb66018c400d57edfbcc1fd23a7eefe3c00c05d6
Daizenghao <3184018779@qq.com>
2023-07-02 11:09:25 +0800

    界面优化

--- app/src/main/java/com/example/wechatproject/util/DBHelper.java
+++ app/src/main/java/com/example/wechatproject/util/DBHelper.java
@@ -93,6 +93,7 @@ public class DBHelper extends SQLiteOpenHelper {
         }
 
         if (cursor != null)
+            //
             cursor.close();
         //System.out.println(contactResult.get(0).getUserName());
 
